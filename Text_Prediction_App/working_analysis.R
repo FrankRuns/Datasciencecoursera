fb1a1dd72a367922cd1b836360803d04bad03720
Frank Corrigan <franciscorrigan3@gmail.com>
2014-12-03 18:29:06 -0500

    updating working analysis not using tm and removing previously created gram files

--- Text_Prediction_App/working_analysis.R
+++ Text_Prediction_App/working_analysis.R
@@ -1,27 +1,116 @@
+# using tm was incredibly slow and couldn't handle a big chunk of data.. 3% was a stretch
+# so I've moved that below and am trying out the stylo and slam package with 10% of the txt data
+
 setwd("/Users/frankCorrigan/Desktop/en_US/")
 
 # read news file
 newsfile <- "en_US.news.txt"
-lineCount <- length(readLines(newsfile))
+# lineCount <- length(readLines(newsfile))
+lineCount <- 1010242
 con <- file(newsfile, "r")
-news <- readLines(con, lineCount*0.001)
+news <- readLines(con, lineCount*0.01)
 close(con)
 
 # read blogs file
 blogsfile <- "en_US.blogs.txt"
-lineCount <- length(readLines(blogsfile))
+# lineCount <- length(readLines(blogsfile))
+lineCount <- 899288
 con <- file(blogsfile, "r")
-blogs <- readLines(con, lineCount*0.001)
+blogs <- readLines(con, lineCount*0.01)
 close(con)
 
 # read twitter file
 twitterfile <- "en_US.twitter.txt"
-lineCount <- length(readLines(twitterfile))
+# lineCount <- length(readLines(twitterfile))
+lineCount <- 2360148
 con <- file(twitterfile, "r")
-tweets <- readLines(con, lineCount*0.001)
+tweets <- readLines(con, lineCount*0.01)
 close(con)
 
 setwd("/Users/frankCorrigan/Datasciencecoursera/Text_Prediction_App/")
+
+# combine all lines into 1 character vector
+allLines <- c(news, blogs, tweets)
+
+# turn everything to lower case and remove punctuaion and numbers
+allLines <- gsub("[[:digit:]]", "", allLines)
+allLines <- gsub("([[:punct:]])", "", tolower(allLines))
+
+# use remove white space
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+allLines <- trim(allLines)
+
+# helper function to split a string by space and convert to character from list
+splitUnlist <- function(text){
+      return (unlist(strsplit(text, " ")))
+}
+
+# make ngrams, use table for counting, and convert to data.table
+library(stylo)
+
+# nGrams <- make.ngrams(allWords, 5)
+allWords <- unlist(strsplit(allLines, " "))
+
+fiveGrams <- make.ngrams(allWords, 5)
+fourGrams <- make.ngrams(allWords, 4)
+threeGrams <- make.ngrams(allWords, 3)
+twoGrams <- make.ngrams(allWords, 2)
+
+fiveGT <- table(fiveGrams)
+fourGT <- table(fourGrams)
+threeGT <- table(threeGrams)
+twoGT <- table(twoGrams)
+
+fiveDF <- as.data.frame(fiveGT, stringsAsFactors = FALSE)
+fourDF <- as.data.frame(fourGT, stringsAsFactors = FALSE)
+threeDF <- as.data.frame(threeGT, stringsAsFactors = FALSE)
+twoDF <- as.data.frame(twoGT, stringsAsFactors = FALSE)
+
+fiveDF <- subset(fiveDF, fiveDF$Freq > 1)
+fiveDF$fiveGrams <- trim(fiveDF$fiveGrams)
+fiveDF$holder <- lapply(fiveDF$fiveGrams, splitUnlist)
+
+for (i in 1:length(fiveDF$holder)){
+      c <- fiveDF$holder[[i]]
+      fiveDF$holder[[i]] <- c[c != ""]
+}
+
+fiveDF$count <- lapply(fiveDF$holder, length)
+fiveDF <- subset(fiveDF, fiveDF$count == 5)
+
+for (i in 1:length(fiveDF$holder)) {
+      fiveDF$holder[[i]] <- paste(fiveDF$holder[[i]], collapse = " ")
+}
+
+# keeping holder for now but ditching the count
+# TODO: some 5+ word phrases still exist... need to kill
+fiveDF$count <- NULL
+
+fourDF <- subset(fourDF, fourDF$Freq > 1)
+fourDF$fourGrams <- trim(fourDF$fourGrams)
+fourDF$holder <- lapply(fourDF$fourGrams, splitUnlist)
+fourDF$count <- lapply(fourDF$holder, length)
+fourDF <- subset(fourDF, fourDF$count == 4)
+fourDF$holder <- NULL; fourDF$count <- NULL
+
+threeDF <- subset(threeDF, threeDF$Freq > 1)
+threeDF$threeGrams <- trim(threeDF$threeGrams)
+threeDF$holder <- lapply(threeDF$threeGrams, splitUnlist)
+threeDF$count <- lapply(threeDF$holder, length)
+threeDF <- subset(threeDF, threeDF$count == 3)
+threeDF$holder <- NULL; threeDF$count <- NULL
+
+twoDF <- subset(twoDF, twoDF$Freq > 1)
+twoDF$twoGrams <- trim(twoDF$twoGrams)
+# TODO: remove all ngrams that are less than 2 grams
+
+fynd <- function(text) {
+      l <- length(splitUnlist(text))
+      outcome <- splitUnlist(grep(paste0("^", text), twoDF$twoGrams, value=TRUE)[1])[l+1]
+      return (outcome)
+}
+
+_______
 
 # create 'giant' corpus with tm package
 library(tm)
@@ -30,7 +119,8 @@ corpus <- Corpus(VectorSource(c(news, blogs, tweets)))
 # clean corpus
 korpus <- tm_map(corpus, tolower)
 korpus <- tm_map(korpus, removePunctuation)
-korpus <- tm_map(korpus, removeNumbers)
+# I don't think I want to remove #'s.. TODO: rethink this
+# korpus <- tm_map(korpus, removeNumbers)
 
 # set options for tdm creation
 options(mc.cores=1)
@@ -55,9 +145,14 @@ fourSum <- as.data.frame(sort(row_sums(tdmFour), decreasing=TRUE))
 threeSum <- as.data.frame(sort(row_sums(tdmThree), decreasing=TRUE))
 twoSum <- as.data.frame(sort(row_sums(tdmTwo), decreasing=TRUE))
 
-# helper function to split a string by space and convert to character from list
-splitUnlist <- function(text){
-      return (unlist(strsplit(text, " ")))
+findText <- function(text) {
+      output <- subDF[subDF$nGrams == grep(paste0("^", text), subDF$nGrams, value=TRUE)[1], ]
+      return (output)
+}
+
+getTwo <- function(text){
+      output <- subDF[subDF$nGrams == grep(paste0("^", text), subDF$nGrams, value=TRUE)[1], ]
+      return (output$nGrams)
 }
 
 # getSecond takes 1 word as input and predicts the next word
