513331ebe6963499b9ffd3cb2dc2fb9daff14c80
Frank Corrigan <franciscorrigan3@gmail.com>
2014-12-04 05:17:27 -0500

    updating working analysis. learning how to leverage grep at least...

--- Text_Prediction_App/working_analysis.R
+++ Text_Prediction_App/working_analysis.R
@@ -61,174 +61,163 @@ fourGT <- table(fourGrams)
 threeGT <- table(threeGrams)
 twoGT <- table(twoGrams)
 
+# create and clean up five gram DF
 fiveDF <- as.data.frame(fiveGT, stringsAsFactors = FALSE)
-fourDF <- as.data.frame(fourGT, stringsAsFactors = FALSE)
-threeDF <- as.data.frame(threeGT, stringsAsFactors = FALSE)
-twoDF <- as.data.frame(twoGT, stringsAsFactors = FALSE)
-
 fiveDF <- subset(fiveDF, fiveDF$Freq > 1)
 fiveDF$fiveGrams <- trim(fiveDF$fiveGrams)
 fiveDF$holder <- lapply(fiveDF$fiveGrams, splitUnlist)
-
 for (i in 1:length(fiveDF$holder)){
       c <- fiveDF$holder[[i]]
       fiveDF$holder[[i]] <- c[c != ""]
 }
-
 fiveDF$count <- lapply(fiveDF$holder, length)
 fiveDF <- subset(fiveDF, fiveDF$count == 5)
-
 for (i in 1:length(fiveDF$holder)) {
       fiveDF$holder[[i]] <- paste(fiveDF$holder[[i]], collapse = " ")
 }
-
 # keeping holder for now but ditching the count
 # TODO: some 5+ word phrases still exist... need to kill
 fiveDF$count <- NULL
 
+# create and clean up four gram DF
+fourDF <- as.data.frame(fourGT, stringsAsFactors = FALSE)
 fourDF <- subset(fourDF, fourDF$Freq > 1)
 fourDF$fourGrams <- trim(fourDF$fourGrams)
 fourDF$holder <- lapply(fourDF$fourGrams, splitUnlist)
+for (i in 1:length(fourDF$holder)){
+      c <- fourDF$holder[[i]]
+      fourDF$holder[[i]] <- c[c != ""]
+}
 fourDF$count <- lapply(fourDF$holder, length)
 fourDF <- subset(fourDF, fourDF$count == 4)
-fourDF$holder <- NULL; fourDF$count <- NULL
+for (i in 1:length(fourDF$holder)) {
+      fourDF$holder[[i]] <- paste(fourDF$holder[[i]], collapse = " ")
+}
+# keeping holder for now but ditching the count
+# TODO: some 5+ word phrases still exist... need to kill
+fourDF$count <- NULL
 
-threeDF <- subset(threeDF, threeDF$Freq > 1)
+# create and clean three gram DF
+threeDF <- as.data.frame(threeGT, stringsAsFactors = FALSE)
+threeDF <- subset(threeDF, threeDF$Freq > 5)
 threeDF$threeGrams <- trim(threeDF$threeGrams)
 threeDF$holder <- lapply(threeDF$threeGrams, splitUnlist)
+for (i in 1:length(threeDF$holder)){
+      c <- threeDF$holder[[i]]
+      threeDF$holder[[i]] <- c[c != ""]
+}
 threeDF$count <- lapply(threeDF$holder, length)
 threeDF <- subset(threeDF, threeDF$count == 3)
-threeDF$holder <- NULL; threeDF$count <- NULL
+for (i in 1:length(threeDF$holder)) {
+      threeDF$holder[[i]] <- paste(threeDF$holder[[i]], collapse = " ")
+}
+# keeping holder for now but ditching the count
+# TODO: some 5+ word phrases still exist... need to kill
+threeDF$count <- NULL
 
-twoDF <- subset(twoDF, twoDF$Freq > 1)
+# create and clean two gram DF
+twoDF <- as.data.frame(twoGT, stringsAsFactors = FALSE)
+twoDF <- subset(twoDF, twoDF$Freq > 10)
 twoDF$twoGrams <- trim(twoDF$twoGrams)
-# TODO: remove all ngrams that are less than 2 grams
+twoDF$holder <- lapply(twoDF$twoGrams, splitUnlist)
+for (i in 1:length(twoDF$holder)){
+      c <- twoDF$holder[[i]]
+      twoDF$holder[[i]] <- c[c != ""]
+}
+twoDF$count <- lapply(twoDF$holder, length)
+twoDF <- subset(twoDF, twoDF$count == 2)
+for (i in 1:length(twoDF$holder)) {
+      twoDF$holder[[i]] <- paste(twoDF$holder[[i]], collapse = " ")
+}
+# keeping holder for now but ditching the count
+# TODO: some 5+ word phrases still exist... need to kill
+twoDF$count <- NULL
 
-fynd <- function(text) {
-      l <- length(splitUnlist(text))
-      outcome <- splitUnlist(grep(paste0("^", text), twoDF$twoGrams, value=TRUE)[1])[l+1]
-      return (outcome)
+# build the finder function
+input <- "this is a" # test
+
+search the 5 data frame for grep("this is a", value = TRUE)
+take the first result in that subset and return the 5th word in that result
+
+find2 <- function(text) {
+      result <- splitUnlist(grep(paste0("^", text), twoDF$twoGrams, value=TRUE)[1])[2] 
+      return (result)
 }
 
-_______
-
-# create 'giant' corpus with tm package
-library(tm)
-corpus <- Corpus(VectorSource(c(news, blogs, tweets)))
-
-# clean corpus
-korpus <- tm_map(corpus, tolower)
-korpus <- tm_map(korpus, removePunctuation)
-# I don't think I want to remove #'s.. TODO: rethink this
-# korpus <- tm_map(korpus, removeNumbers)
-
-# set options for tdm creation
-options(mc.cores=1)
-
-# declare 4-gram, 3-gram, 2-gram tokenizers using RWeka
-require(RWeka)
-fiveGramizer <- function(x) NGramTokenizer(x, Weka_control(min=5, max=5))
-fourGramizer <- function(x) NGramTokenizer(x, Weka_control(min=4, max=4))
-threeGramizer <- function(x) NGramTokenizer(x, Weka_control(min=3, max=3))
-twoGramizer <- function(x) NGramTokenizer(x, Weka_control(min=2, max=2))
-
-# create term document matrices
-tdmFive <- TermDocumentMatrix(korpus, control=list(tokenize=fiveGramizer))
-tdmFour <- TermDocumentMatrix(korpus, control=list(tokenize=fourGramizer))
-tdmThree <- TermDocumentMatrix(korpus, control=list(tokenize=threeGramizer))
-tdmTwo <- TermDocumentMatrix(korpus, control=list(tokenize=twoGramizer))
-
-# calc and save row sums with slam's row_sums
-library(slam)
-fiveSum <- as.data.frame(sort(row_sums(tdmFive), decreasing=TRUE))
-fourSum <- as.data.frame(sort(row_sums(tdmFour), decreasing=TRUE))
-threeSum <- as.data.frame(sort(row_sums(tdmThree), decreasing=TRUE))
-twoSum <- as.data.frame(sort(row_sums(tdmTwo), decreasing=TRUE))
-
-findText <- function(text) {
-      output <- subDF[subDF$nGrams == grep(paste0("^", text), subDF$nGrams, value=TRUE)[1], ]
-      return (output)
+find3 <- function(text) {
+      result <- splitUnlist(grep(paste0("^", text), threeDF$threeGrams, value=TRUE)[1])[3]
+      return (result)
 }
 
-getTwo <- function(text){
-      output <- subDF[subDF$nGrams == grep(paste0("^", text), subDF$nGrams, value=TRUE)[1], ]
-      return (output$nGrams)
+find4 <- function(text) {
+      result <- splitUnlist(grep(paste0("^", text), fourDF$fourGrams, value=TRUE)[1])[4]
+      return (result)
 }
 
-# getSecond takes 1 word as input and predicts the next word
-getSecond <- function(text){
-      outcome <- splitUnlist(grep(paste0("^", text), rownames(twoSum), value=TRUE)[1])[2]
-      return (outcome)
+find5 <- function(text) {
+      result <- splitUnlist(grep(paste0("^", text), fiveDF$fiveGrams, value=TRUE)[1])[5]
+      return (result)
 }
 
-# getThird takes a string of 2 words are input and predicts the next word
-getThird <- function(text){
-      # test is "a lot"
-      outcome <- splitUnlist(grep(paste0("^", text), rownames(threeSum), value=TRUE)[1])[3]
-      # test is "going fast"
-      if (is.na(outcome)){
-            outcome <- getSecond(splitUnlist(text)[2])
-      }      
-      if (is.na(outcome)){
-            outcome <- "i'm failing to predict a next word because your level of intelligence is intimidating me"
+# 'day' works... predicts 'when'
+get2 <- function(text) {
+      phrase <- splitUnlist(grep(text, fourDF$phrase, value=TRUE)[1])
+      pos <- grep(text, phrase)
+      if (pos == 5) {
+            phrase <- splitUnlist(grep(text, fourDF$phrase, value=TRUE)[2])
+            pos <- grep(text, phrase)
       }
-      return (outcome)
+      if (pos == 5) {
+            phrase <- splitUnlist(grep(text, fourDF$phrase, value=TRUE)[3])
+            pos <- grep(text, phrase)
+      }
+      word <- phrase[pos+1]
+      return (word)
 }
 
-# getFourth takes a string of 3 words as input and predicts the next word
-getFourth <- function(text){
-      outcome <- splitUnlist(grep(paste0("^", text), rownames(fourSum), value=TRUE)[1])[4]
-      if (is.na(outcome)){
-            outcome <- getThird(paste(splitUnlist(text)[2], splitUnlist(text)[3]))
-      }
-      if (is.na(outcome)){
-            outcome <- getSecond(splitUnlist(text)[3])
-      }
-      if (is.na(outcome)){
-            outcome <- "i'm failing to predict a next word because you're good looks are distracting me"
-      }
-      return (outcome)
+# five / four
+# baby: NA, baby: i
+# go: to, NA
+# great: deal, day
+
+get3 <- function(text) {
+      phrase <- splitUnlist(grep(paste0("^", text), threeDF$phrase, value=TRUE)[4])
+      pos <- grep(splitUnlist(text)[2], phrase)
+      word <- phrase[pos+1]
+      return (word)
 }
 
-# getFifth takes a string of 4 words as input and predicts the next word 
-getFifth <- function(text){
-      outcome <- splitUnlist(grep(paste0("^", text), rownames(fiveSum), value=TRUE)[1])[5]
-      if (is.na(outcome)){
-            outcome <- getFourth(paste(splitUnlist(text)[2], splitUnlist(text)[3], splitUnlist(text)[4]))
-      }
-      if (is.na(outcome)){
-            outcome <- getThird(paste(splitUnlist(text)[3], splitUnlist(text)[4]))
-      }
-      if (is.na(outcome)){
-            outcome <- getSecond(splitUnlist(text)[4])
-      }
-      if (is.na(outcome)){
-            outcome <- "i'm failing to predict a next word because i'm having a bad day... sorry."
-      }
-      return (outcome)
-}
+input <- "cant wait to"
+phrase <- splitUnlist(grep(input, fiveDF$fiveGrams, value=TRUE)[1])
+pos <- grep(splitUnlist(input)[3], phrase)
+word <- phrase[pos+1]
 
-# finder takes any string as input and predicts the next word
-finder <- function(text){
-      # find the number of words in the string
+input <- "amazing cant wait to"
+phrase <- splitUnlist(grep(paste0("^", input), fiveDF$fiveGrams, value=TRUE)[1])
+pos <- grep(splitUnlist(phrase)[4], phrase)
+word <- phrase[pos+1]
+
+f <- function(text) {
       nWords <- length(splitUnlist(text))
-      # if nWords is > 4, take only the last four words to predict the next word
-      # TODO: clean this up, it looks like shit
-      if (nWords > 4) {
+      if (nWords >= 5) {
             start <- length(unlist(strsplit(text, " "))) - 3
             end <- length(unlist(strsplit(text, " ")))
             text <- toString(unlist(strsplit(text, " "))[start:end])
             text <- gsub(",", "", text)
-            outcome <- getFifth(text)
+            outcome <- find5(text)
       }
-      # if nWords = 4 use getFifth(text)
-      if (nWords == 4) {outcome <- getFifth(text) }
-      # if nWords == 3 use getFourth(text)
-      if (nWords == 3) {outcome <- getFourth(text) }
-      # if nWords == 2 use getThird(text)
-      if (nWords == 2) { outcome <- getThird(text) }
-      # if there is 1 word run the getSecond function
-      if (nWords == 1) { outcome <- getSecond(text) }
+      if (nWords == 4) {
+            outcome <- find5(text)
+      }
+      if (nWords == 3) {
+            outcome <- find4(text)
+      }
+      if (nWords == 2) {
+            outcome <- find3(text)
+      }
+      if (nWords == 1) {
+            outcome <- find2(text)
+      }
       return (outcome)
 }
 
